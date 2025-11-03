2762df7774db34426962182a5db941269d144388
ray-d-song <songraysmail@gmail.com>
2025-11-03 17:35:27 +0800

    fzf排除部分目录

--- lua/editor/fzf.lua
+++ lua/editor/fzf.lua
@@ -7,6 +7,21 @@ return {
         height = 0.85,
         width = 0.80,
       },
+      files = {
+        fd_opts = [[--color=never --type f --hidden --follow ]] ..
+                  [[--exclude .git --exclude node_modules --exclude .cache ]] ..
+                  [[--exclude dist --exclude build --exclude target ]] ..
+                  [[--exclude .next --exclude .nuxt --exclude coverage ]] ..
+                  [[--exclude .vscode --exclude .idea --exclude __pycache__]],
+      },
+      grep = {
+        rg_opts = [[--column --line-number --no-heading --color=always ]] ..
+                  [[--smart-case --max-columns=4096 ]] ..
+                  [[--glob '!.git/' --glob '!node_modules/' --glob '!.cache/' ]] ..
+                  [[--glob '!dist/' --glob '!build/' --glob '!target/' ]] ..
+                  [[--glob '!.next/' --glob '!.nuxt/' --glob '!coverage/' ]] ..
+                  [[--glob '!.vscode/' --glob '!.idea/' --glob '!__pycache__/']],
+      },
     })
 
     local fzf = require('fzf-lua')
