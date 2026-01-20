e327451b517d774715748d51fec9142b5c797b85
Ray-D-Song <songraysmail@gmail.com>
2026-01-20 14:12:23 +0800

    feat: format js/ts on save

--- lua/lsp/js-ts.lua
+++ lua/lsp/js-ts.lua
@@ -1,6 +1,21 @@
 return function(capabilities, on_attach)
   vim.lsp.enable('ts_ls')
   vim.lsp.config('ts_ls', {
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+      -- 1. ensure keymap
+      on_attach(client, bufnr)
+
+      -- 2. format on save
+      if client.supports_method('textDocument/formatting') then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+          buffer = bufnr,
+          callback = function()
+            vim.lsp.buf.format({ async = false, bufnr = bufnr })
+          end,
+        })
+      end
+    end,
     settings = {
     },
   })
